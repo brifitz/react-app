9a3e47d6f4635348520bc4d7a71fca4130a52e91
Brian <brian@brianfitzpatrick.ie>
2022-10-18 18:08:41 +0100

    feat: new features again

--- release.config.jsx
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/brifitz/react-app",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
-    ]
-}
